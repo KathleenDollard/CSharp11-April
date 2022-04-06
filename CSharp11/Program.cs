5b44e343de42aae9f69b95d11aec5d077f0abdec
Kathleen Dollard <kdollard@microsoft.com>
2022-04-06 16:21:24 -0700

    Add project files.

---
+++ CSharp11/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine(AddAll(1,2,3,5,7,11,13,17));
+
+static int AddAll(params int[] values)
+{
+    if (values == null) throw new ArgumentNullException(nameof(values));
+
+    int result = 0;
+    foreach (var value in values)
+    {
+        result += value;
+    }
+    return result;
+}
