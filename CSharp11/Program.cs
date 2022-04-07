0bb18cf5e4cb790153e11655d0559afeac387a20
Kathleen Dollard <kdollard@microsoft.com>
2022-04-06 18:09:50 -0700

    Added raw string wrapper

--- CSharp11/Program.cs
+++ CSharp11/Program.cs
@@ -1,6 +1,11 @@
 ﻿using CSharp11;
 
-Console.WriteLine(AddNumerics.AddAll(1,2,3,5,7,11,13,17, 19));
-TestParamNameAttribute.Test< AddNumerics>();
+Console.WriteLine(AddNumerics.AddAll(1, 2, 3, 5, 7, 11, 13, 17, 19));
+TestParamNameAttribute.Test<AddNumerics>();
 
 Console.WriteLine(ListPatterns.Show(args));
+
+foreach (var c in RawStrings.Show())
+{
+    Console.WriteLine(c);
+}
---
+++ CSharp11/RawStrings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp11
+{
+    public class RawStrings
+    {
+        public static IEnumerable<string> Show()
+        => new List<string>
+        {
+        };
+    }
+}
