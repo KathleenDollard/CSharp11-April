566bfc6ec6a957b293e44ea0842c4ed264035c68
Kathleen Dollard <kdollard@microsoft.com>
2022-04-12 14:35:43 -0700

    after talk

--- CSharp11/AddNumerics.cs
+++ CSharp11/AddNumerics.cs
@@ -2,11 +2,15 @@
 {
     public class AddNumerics
     {
-        [ParamName("values")]
-        public static int AddAll(params int[] values)
-        {
-            if (values == null) throw new ArgumentNullException(nameof(values));
 
+        // I love it
+        // Like the feature, some other syntax
+        // don't do the feature now
+
+        [ParamName("values")]
+        public static T AddAll<T>(params T[] values!!)
+            where T : INumber<T>
+        {
             int result = 0;
             foreach (var value in values)
             {
